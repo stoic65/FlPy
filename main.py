bbe1a12a806548f35f720b2a75e97279b241c228
Varun <varun95.gm@gmail.com>
2016-04-30 21:00:26 +0530

    Added textures

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
 from kivy.clock import Clock
 import time
+from kivy.uix.boxlayout import BoxLayout
 class FlpyApp(App):
+    pass
+class Shelf(BoxLayout):
     pass
 
 if __name__ == '__main__':
